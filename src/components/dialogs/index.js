0fdfac0e185ce4b7ca0bfba460b619fa9398e93b
ShinaBR2 <ShinaBR2@gmail.com>
2023-04-28 11:23:55 +0700

    Move dialog components

---
+++ src/components/dialogs/index.js
@@ -0,0 +1,7 @@
+import AuthDialog from "./AuthDialog";
+import ShareDialog from "./ShareDialog";
+
+export {
+  AuthDialog,
+  ShareDialog
+}
--- src/components/movies/index.js
+++ src/components/movies/index.js
@@ -1,9 +1,7 @@
 import MovieList from './MovieList';
 import MovieItem from './MovieItem';
-import ShareDialog from './ShareDialog';
 
 export {
   MovieList,
   MovieItem,
-  ShareDialog
 }
