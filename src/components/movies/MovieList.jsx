fdc4f5712bfc1bab89112088c708298bdd556373
ShinaBR2 <ShinaBR2@gmail.com>
2023-04-28 13:22:43 +0700

    Loading movies

--- src/components/movies/MovieList.jsx
+++ src/components/movies/MovieList.jsx
@@ -1,8 +1,18 @@
-import { Container, Typography } from "@mui/material";
+import { Container, Grid, LinearProgress, Typography } from "@mui/material";
 import MovieItem from "./MovieItem";
 
 const MovieList = (props) => {
-  const { list } = props;
+  const { list, isLoading } = props;
+
+  if (isLoading || 1) {
+    return (
+      <Container>
+        <Grid container direction="column" justifyContent="center" sx={{ width: '100%', height: "150px" }}>
+          <LinearProgress />
+        </Grid>
+      </Container>
+    )
+  }
 
   if (!list || !list.length) {
     return (
