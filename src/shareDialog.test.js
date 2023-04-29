cb652903a0be5cdafadb4325faeea912fe468d33
ShinaBR2 <ShinaBR2@gmail.com>
2023-04-29 17:25:24 +0700

    Cleanup

--- src/shareDialog.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-
-describe("Share Movie Dialog", () => {
-  it("should render dialog correctly", () => {});
-  it("should show error when no url entered", () => {});
-  it("should show success message when enter url", () => {});
-  it("should close share video dialog upon success", () => {});
-});
